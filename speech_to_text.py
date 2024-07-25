ce4d83f5764b3613768a87d425a6a0226e75d0e2
salamandermartin <salamancam.programming@gmail.com>
2024-07-25 10:38:13 -0400

    tts

--- speech_to_text.py
+++ speech_to_text.py
@@ -14,3 +14,10 @@ class SpeechToTextManager:
             exit('No environment variables found for AZURE_TTS')
 
         self.azure_speechconfig.speech_recognition_language = "en-US"
+
+    def speechtotext_from_mic(self):
+        self.azure_audioconfig = speechsdk.audio.AudioConfig(use_default_microphone= True)
+        self.azure_speechrecognizer = speechsdk.SpeechRecognizer(speech_config=self.azure_speechconfig, audio_config=self.azure_audioconfig)
+
+        speech_recognition_result = self.azure_speechrecognizer.recognize_once_async().get()
+        text_result = speech_recognition_result.text
